f4a35b3ff1bcd07a49e9eb4e886c98a5b5aeaa5d
Kai Zhang <kai@kzhang.org>
2016-12-07 20:24:39 -0800

    do not generate bed files

--- app/Component/ATACSeq.hs
+++ app/Component/ATACSeq.hs
@@ -13,6 +13,7 @@ import           Bio.Pipeline.CallPeaks
 import           Bio.Pipeline.NGS
 import           Control.Lens
 import           Control.Monad.IO.Class    (liftIO)
+import qualified Data.Text                 as T
 import           Scientific.Workflow
 
 import           Constants
@@ -48,28 +49,30 @@ builder = do
     [ "Get_ATAC_data", "ATAC_alignment"] ~> "ATAC_bam_filt_prepare"
     path ["ATAC_bam_filt_prepare", "ATAC_bam_filt", "ATAC_remove_dups"]
 
-    node "ATAC_makeBED_prepare" [| \(oriInput, input) -> do
-        let filtInput = concatMap splitExpByFile $ filterExpByFile
+    node "ATAC_callpeaks_prepare" [| \(oriInput, input) -> do
+        let filtInput = filterExpByFile
                 (\x -> formatIs BamFile x && "filtered" `elem` x^._Single.tags)
                 oriInput
-        return $ filtInput ++ input
+        return $ mergeExps $ filtInput ++ input
         |] $ submitToRemote .= Just False
-    [ "Get_ATAC_data", "ATAC_remove_dups"] ~> "ATAC_makeBED_prepare"
+    [ "Get_ATAC_data", "ATAC_remove_dups"] ~> "ATAC_callpeaks_prepare"
 
+{-
     node "ATAC_makeBED" [| \x -> bam2Bed <$> atacOutput <*> return x >>= liftIO
         |] $ batch .= 1 >> stateful .= True
-
     node "ATAC_combine_reps_prepare" [| return . mergeExps |] $
         submitToRemote .= Just False
     node "ATAC_combine_reps" [| \x -> mergeReplicatesBed <$>
         atacOutput <*> return x >>= liftIO
         |] $ batch .= 1 >> stateful .= True
+        -}
 
-    node "ATAC_callpeaks_prepare" [| mapM $ \x -> return (x, Nothing) |] $
-        submitToRemote .= Just False
-    node "ATAC_callpeaks" [| \x -> callPeaks <$> atacOutput <*>
-        return (return ()) <*> return x >>= liftIO
+    node "ATAC_callpeaks" [| \e -> do
+        dir <- atacOutput
+        let input = e^..replicates.folded.files.folded.filtered (formatIs BamFile)
+            output = dir ++ "/" ++ T.unpack (e^.eid) ++ "_rep0.NarrowPeak"
+        fl <- liftIO $ callPeaks output input [] (pair .= pairedEnd e)
+        return $ replicates .~ [files .~ [fl] $ emptyReplicate] $ e
         |] $ batch .= 1 >> stateful .= True
 
-    path [ "ATAC_makeBED_prepare", "ATAC_makeBED", "ATAC_combine_reps_prepare"
-         , "ATAC_combine_reps", "ATAC_callpeaks_prepare", "ATAC_callpeaks" ]
+    path [ "ATAC_callpeaks_prepare", "ATAC_callpeaks" ]
