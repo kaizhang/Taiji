de9b4d201bcb88ca7732c4dd34c81a4efc016fec
Kai Zhang <kai@kzhang.org>
2017-01-26 14:50:08 -0800

    some preliminary work on visualization

--- taiji-viz/app/Main.hs
+++ taiji-viz/app/Main.hs
@@ -1,19 +1,27 @@
 module Main where
 
-import qualified Data.ByteString as B
+import qualified Data.ByteString.Char8 as B
 import Taiji.Visualize
 import System.Environment
+import qualified Data.HashMap.Strict as M
 import qualified Data.Vector.Unboxed as U
 import Diagrams.Prelude
 import Diagrams.Backend.Rasterific
+import Data.List
+import Data.Function
 
 main :: IO ()
 main = do
-    [fl] <- getArgs
-    c <- B.readFile fl
+    [f1,f2] <- getArgs
+    c1 <- B.readFile f1
+    c2 <- B.readFile f2
 
-    let xs' = map logFoldChange $ readTSV c
+    let m1 = readTSV c1
+        m2 = readTSV c2
+        (labels, xs) = unzip $ map unzip $ groupBy ((==) `on` (fst.fst)) $ sort $
+            M.toList $ M.intersectionWith (,) m1 m2
         w = width dia
         h = height dia
-        dia = spotPlot xs' xs'
-    renderRasterific "out.png" (dims2D 200 (200*(h/w))) dia
+        dia = spotPlot (map B.unpack $ fst $ unzip $ map head labels)
+            (map B.unpack $ snd $ unzip $ head $ labels) xs
+    renderRasterific "out.png" (dims2D 800 (800*(h/w))) dia
--- taiji-viz/app/Taiji/Visualize.hs
+++ taiji-viz/app/Taiji/Visualize.hs
@@ -1,29 +1,51 @@
-{-# LANGUAGE GADTs #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE GADTs            #-}
 
 module Taiji.Visualize where
 
-import qualified Data.ByteString.Char8 as B
-import Diagrams.Prelude
-import Diagrams.Backend.Rasterific
-import qualified Data.Vector as V
-import Data.ByteString.Lex.Fractional
-import Data.Maybe
-import Bio.Utils.Functions (ihs')
+import           Bio.Utils.Functions            (ihs')
+import           Bio.Utils.Misc                 (readDouble)
+import qualified Data.ByteString.Char8          as B
+import           Data.ByteString.Lex.Fractional
+import           Data.Colour                    (blend)
+import qualified Data.HashMap.Strict            as M
+import           Data.Maybe
+import qualified Data.Vector                    as V
+import           Diagrams.Backend.Rasterific
+import           Diagrams.Prelude
 
-spotPlot :: [[Double]]  -- ^ size
-         -> [[Double]]  -- ^ color
+spotPlot :: [String]  -- ^ row labels
+         -> [String]  -- ^ column labels
+         -> [[(Double, Double)]]  -- ^ size and color
          -> Diagram B
-spotPlot ss cs = vcat $ map (hcat . map mkCircle) ss
+spotPlot rowlab collab = vsep 1 . (header:) . zipWith g rowlab . map draw
   where
-    mkCircle x = circle x # lw 0 # fc red
+    draw dat = hsep 1 $ zipWith mkCircle ranks' expr'
+      where
+        ranks' = linearMap (2, 15) $ map log ranks
+        expr' = linearMap (0, 1) $ map ihs' expr
+        (ranks, expr) = unzip dat
+    header = alignR $ hsep 1 $ map (\x ->
+        (alignB $ center $ scale 6 $ texterific x # rotate (90 @@ deg)) <> box) collab
+    mkCircle x y = withEnvelope box $ circle x # lw 0 # fc (blend y red white)
+    g lab x = alignR $ center (scale 6 $ texterific lab) ||| strutX 2 ||| x
+    box = circle 15 # lw 0 :: Diagram B
 
-readTSV :: B.ByteString -> [[Double]]
-readTSV input = map f $ tail $ B.lines input
+readTSV :: B.ByteString -> M.HashMap (B.ByteString, B.ByteString) Double
+readTSV input = M.fromList $ concatMap (f . B.split '\t') content
   where
-    f = map (fst . fromJust . readSigned readDecimal) . tail . B.split '\t'
+    f (x:xs) = zipWith (\s v -> ((x,s), readDouble v)) samples xs
+    (header:content) = B.lines input
+    samples = tail $ B.split '\t' header
 
 logFoldChange :: [Double] -> [Double]
 logFoldChange xs = map (ihs' . (/ m)) xs
   where
     m = maximum xs
+
+linearMap :: (Double, Double) -> [Double] -> [Double]
+linearMap (lo, hi) xs = map f xs
+  where
+    f x = lo + (x - min') / (max' - min') * (hi - lo)
+    min' = minimum xs
+    max' = maximum xs
