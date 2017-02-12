391e9691a20bcad6bcc8191473f2d55c9f3ee024
Kai Zhang <kai@kzhang.org>
2017-02-12 01:22:42 -0800

    work on viz

--- taiji-viz/app/Main.hs
+++ taiji-viz/app/Main.hs
@@ -4,8 +4,10 @@ import           AI.Clustering.Hierarchical
 import           Bio.Utils.Functions         (ihs')
 import qualified Data.ByteString.Char8       as B
 import           Data.Function
-import qualified Data.HashMap.Strict         as M
+import qualified Data.HashMap.Strict         as HM
 import           Data.List
+import qualified Data.Matrix                 as M
+import           Data.Ord                    (comparing)
 import qualified Data.Vector                 as V
 import qualified Data.Vector.Unboxed         as U
 import           Diagrams.Backend.Rasterific
@@ -20,24 +22,21 @@ main = do
     c1 <- B.readFile f1
     c2 <- B.readFile f2
 
-    let m1 = readTSV c1
-        m2 = readTSV c2
+    let m1 = fmap ihs' $ readTSV c1
+        m2 = fmap ihs' $ readTSV c2
         (labels, xs) = unzip $ map unzip $ groupBy ((==) `on` (fst.fst)) $ sort $
-            M.toList $ M.intersectionWith (,) m1 m2
+            HM.toList $ HM.intersectionWith (,) m1 m2
         rowlab = map B.unpack $ fst $ unzip $ map head labels
         collab = map B.unpack $ snd $ unzip $ head $ labels
-        distFn x y = euclidean (V.fromList $ fst $ unzip $ snd x)
-            (V.fromList $ fst $ unzip $ snd y)
-        (rowlab', xs') = unzip $ flatten $ hclust Ward
-            (V.fromList $ filter f $ zip rowlab $ map (map (\(x,y) -> (ihs' x, ihs' y))) xs)
-            distFn
-        (collab', xs'') = unzip $ flatten $ hclust Ward
-            (V.fromList $ zip collab $ transpose xs') distFn
+        table = filterRows f $ Table rowlab collab $ M.fromLists xs
+        distFn x y = euclidean (fst $ V.unzip $ snd x) (fst $ V.unzip $ snd y)
+        table' = reorderColumns (sortBy (comparing fst)) $
+            reorderRows (\x -> flatten $ hclust Ward (V.fromList x) distFn) table
         w = width dia
         h = height dia
         n = fromIntegral $ length (head xs) * 50
-        dia = spotPlot rowlab' collab' $ transpose xs''
+        dia = spotPlot table'
     renderRasterific "out.png" (dims2D n (n*(h/w))) dia
   where
-    f (_, xs) = let (m, v) = meanVarianceUnb $ V.fromList $ fst $ unzip xs
+    f (_, xs) = let (m, v) = meanVarianceUnb $ fst $ V.unzip xs
                 in sqrt v / m > 1
--- taiji-viz/app/Taiji/Visualize.hs
+++ taiji-viz/app/Taiji/Visualize.hs
@@ -13,12 +13,40 @@ import           Data.Maybe
 import qualified Data.Vector                    as V
 import           Diagrams.Backend.Rasterific
 import           Diagrams.Prelude
+import qualified Data.Matrix as M
 
-spotPlot :: [String]  -- ^ row labels
-         -> [String]  -- ^ column labels
-         -> [[(Double, Double)]]  -- ^ size and color
-         -> Diagram B
-spotPlot rowlab collab = vsep 1 . (header:) . zipWith g rowlab . map draw
+data Table a = Table
+    { rowNames :: [String]
+    , colNames :: [String]
+    , matrix :: M.Matrix a
+    }
+
+type ReodrderFn a = [(String, V.Vector a)] -> [(String, V.Vector a)]
+
+filterRows :: ((String, V.Vector a) -> Bool) -> Table a -> Table a
+filterRows fn table = table
+    { rowNames = names
+    , matrix = M.fromRows rows }
+  where
+    (names, rows) = unzip $ filter fn $ zip (rowNames table) $ M.toRows $ matrix table
+
+reorderRows :: ReodrderFn a -> Table a -> Table a
+reorderRows fn table = table
+    { rowNames = names
+    , matrix = M.fromRows rows }
+  where
+    (names, rows) = unzip $ fn $ zip (rowNames table) $ M.toRows $ matrix table
+
+reorderColumns :: ReodrderFn a -> Table a -> Table a
+reorderColumns fn table = table
+    { colNames = names
+    , matrix = M.fromColumns cols}
+  where
+    (names, cols) = unzip $ fn $ zip (colNames table) $ M.toColumns $ matrix table
+
+spotPlot :: Table (Double, Double) -> Diagram B
+spotPlot (Table rowlab collab xs) = vsep 1 $ (header:) $ zipWith g rowlab $
+    map draw $ M.toLists xs
   where
     draw dat = hsep 1 $ zipWith mkCircle ranks' expr'
       where
