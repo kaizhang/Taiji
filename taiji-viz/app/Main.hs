186411767e6837abdf44ab93ee75c5791a088c1c
Kai Zhang <kai@kzhang.org>
2017-02-23 14:00:38 -0800

    test

--- taiji-viz/app/Main.hs
+++ taiji-viz/app/Main.hs
@@ -1,42 +1,108 @@
+{-# LANGUAGE BangPatterns #-}
 module Main where
 
 import           AI.Clustering.Hierarchical
 import           Bio.Utils.Functions         (ihs')
 import qualified Data.ByteString.Char8       as B
+import qualified Data.CaseInsensitive        as CI
 import           Data.Function
 import qualified Data.HashMap.Strict         as HM
 import           Data.List
+import Data.List.Split (splitOn)
 import qualified Data.Matrix                 as M
-import           Data.Ord                    (comparing)
+import           Data.Maybe
+import           Data.Ord                    (compare, comparing)
+import           Data.Semigroup              ((<>))
 import qualified Data.Vector                 as V
 import qualified Data.Vector.Unboxed         as U
-import           Diagrams.Backend.Rasterific
-import           Diagrams.Prelude
+import           Diagrams.Backend.Rasterific (renderRasterific)
+import           Diagrams.Prelude (dims2D, width, height)
+import           Options.Applicative
 import           Statistics.Sample           (meanVarianceUnb)
 import           System.Environment
 import           Taiji.Visualize
 
-main :: IO ()
-main = do
-    [f1,f2] <- getArgs
-    c1 <- B.readFile f1
-    c2 <- B.readFile f2
+data Options = Options
+    { input          :: FilePath
+    , output         :: FilePath
+    , rowNamesFilter :: Maybe FilePath
+    , colNamesFilter :: Maybe FilePath
+    , expression     :: FilePath
+    }
 
-    let m1 = fmap ihs' $ readTSV c1
-        m2 = fmap ihs' $ readTSV c2
-        (labels, xs) = unzip $ map unzip $ groupBy ((==) `on` (fst.fst)) $ sort $
-            HM.toList $ HM.intersectionWith (,) m1 m2
-        rowlab = map B.unpack $ fst $ unzip $ map head labels
-        collab = map B.unpack $ snd $ unzip $ head $ labels
-        table = filterRows f $ Table rowlab collab $ M.fromLists xs
-        distFn x y = euclidean (fst $ V.unzip $ snd x) (fst $ V.unzip $ snd y)
-        table' = reorderColumns (sortBy (comparing fst)) $
-            reorderRows (\x -> flatten $ hclust Ward (V.fromList x) distFn) table
+parser :: Parser Options
+parser = Options
+    <$> strArgument
+      ( metavar "INPUT"
+     <> help "PageRank result" )
+    <*> strArgument (metavar "OUTPUT")
+    <*> (optional . strOption)
+      ( long "rowNamesFilter" )
+    <*> (optional . strOption)
+      ( long "colNamesFilter" )
+    <*> strOption
+      ( long "expression" )
+
+defaultMain :: Options -> IO ()
+defaultMain opts = do
+    rank <- (fmap ihs' . readTSV) <$> B.readFile (input opts)
+    expr <- (fmap ihs' . readTSV) <$> B.readFile (expression opts)
+
+    let (labels, xs) = unzip $ map unzip $ groupBy ((==) `on` (fst.fst)) $ sort $
+            HM.toList $ HM.intersectionWith (,) rank expr
+        rowlab = map (B.unpack . CI.original) $ fst $ unzip $ map head labels
+        collab = map (B.unpack . CI.original) $ snd $ unzip $ head $ labels
+        dataTable = filterRows f $ Table rowlab collab $ M.fromLists xs
+
+    dataTable' <- case rowNamesFilter opts of
+        Nothing -> return dataTable
+        Just fl -> do
+            names <- lines <$> readFile fl
+            return $ filterRows (filterByName names) dataTable
+
+    --let table' = reorderColumns (orderByName ["neural-tube", "forebrain", "midbrain", "hindbrain", "heart", "intestine", "kidney", "limb", "liver", "lung", "stomach"]) $
+     --       reorderRows (\x -> flatten $ hclust Ward (V.fromList x) distFn) dataTable'
+    let table' = reorderColumns (sortBy (comparing (last . splitOn "_" . fst))) $
+            reorderRows (\x -> flatten $ hclust Ward (V.fromList x) distFn) dataTable'
         w = width dia
         h = height dia
         n = fromIntegral $ length (head xs) * 50
         dia = spotPlot table'
-    renderRasterific "out.png" (dims2D n (n*(h/w))) dia
+    renderRasterific (output opts) (dims2D n (n*(h/w))) dia
   where
-    f (_, xs) = let (m, v) = meanVarianceUnb $ fst $ V.unzip xs
-                in sqrt v / m > 1
+    distFn x y = euclidean (fst $ V.unzip $ snd x) (fst $ V.unzip $ snd y)
+    f (_, xs) = V.any (>1e-4) xs' && case () of
+        _ | n >= 5 -> let (m, v) = meanVarianceUnb $ fst $ V.unzip xs
+                      in sqrt v / m > 1
+          | otherwise -> V.maximum xs' / V.minimum xs' >= 2.5
+      where
+        n = V.length xs
+        xs' = fst $ V.unzip xs
+
+
+main :: IO ()
+main = defaultMain =<< execParser opts
+  where
+    opts = info (parser <**> helper)
+      ( fullDesc
+     <> header "Taiji-viz - a visualization tool for Taiji" )
+
+orderByName :: [String] -> ReodrderFn a
+orderByName prefix = sortBy $ \(a,_) (b,_) ->
+    let idx1 = findIdx a
+        idx2 = findIdx b
+    in case () of
+        _ | isJust idx1 && isJust idx2 -> case compare idx1 idx2 of
+                LT -> LT
+                GT -> GT
+                EQ -> compare a b
+          | otherwise -> compare a b
+  where
+    findIdx x = go prefix 0
+      where
+        go (y:ys) !i | isPrefixOf y x = Just i
+                     | otherwise = go ys (i+1)
+        go _ _ = Nothing
+
+filterByName :: [String] -> FilterFn a
+filterByName xs = \(x, _) -> x `elem` xs
--- taiji-viz/app/Taiji/Visualize.hs
+++ taiji-viz/app/Taiji/Visualize.hs
@@ -7,23 +7,26 @@ import           Bio.Utils.Functions            (ihs')
 import           Bio.Utils.Misc                 (readDouble)
 import qualified Data.ByteString.Char8          as B
 import           Data.ByteString.Lex.Fractional
+import qualified Data.CaseInsensitive           as CI
 import           Data.Colour                    (blend)
 import qualified Data.HashMap.Strict            as M
+import           Data.List.Split                (chunksOf)
+import qualified Data.Matrix                    as M
 import           Data.Maybe
 import qualified Data.Vector                    as V
 import           Diagrams.Backend.Rasterific
 import           Diagrams.Prelude
-import qualified Data.Matrix as M
 
 data Table a = Table
     { rowNames :: [String]
     , colNames :: [String]
-    , matrix :: M.Matrix a
+    , matrix   :: M.Matrix a
     }
 
 type ReodrderFn a = [(String, V.Vector a)] -> [(String, V.Vector a)]
+type FilterFn a = (String, V.Vector a) -> Bool
 
-filterRows :: ((String, V.Vector a) -> Bool) -> Table a -> Table a
+filterRows :: FilterFn a -> Table a -> Table a
 filterRows fn table = table
     { rowNames = names
     , matrix = M.fromRows rows }
@@ -46,23 +49,26 @@ reorderColumns fn table = table
 
 spotPlot :: Table (Double, Double) -> Diagram B
 spotPlot (Table rowlab collab xs) = vsep 1 $ (header:) $ zipWith g rowlab $
-    map draw $ M.toLists xs
+    map draw $ scaleData $ M.toLists xs
   where
-    draw dat = hsep 1 $ zipWith mkCircle ranks' expr'
+    scaleData dat = chunksOf n $ zip r $ linearMap (4, 16) e
+      where
+        (r, e) = unzip $ concat dat
+        n = length $ head dat
+    draw dat = hsep 1 $ zipWith mkCircle ranks' expr
       where
         ranks' = linearMap (0, 1) ranks
-        expr' = linearMap (2, 15) expr
         (ranks, expr) = unzip dat
     header = alignR $ hsep 1 $ map (\x ->
-        (alignB $ center $ scale 6 $ texterific x # rotate (90 @@ deg)) <> box) collab
+        (alignB $ center $ scale 10 $ texterific x # rotate (90 @@ deg)) <> box) collab
     mkCircle x y = withEnvelope box $ circle y # lw 0 # fc (blend x red white)
-    g lab x = alignR $ center (scale 6 $ texterific lab) ||| strutX 2 ||| x
+    g lab x = alignR $ center (scale 10 $ texterific lab) ||| strutX 5 ||| x
     box = circle 15 # lw 0 :: Diagram B
 
-readTSV :: B.ByteString -> M.HashMap (B.ByteString, B.ByteString) Double
+readTSV :: B.ByteString -> M.HashMap (CI.CI B.ByteString, CI.CI B.ByteString) Double
 readTSV input = M.fromList $ concatMap (f . B.split '\t') content
   where
-    f (x:xs) = zipWith (\s v -> ((x,s), readDouble v)) samples xs
+    f (x:xs) = zipWith (\s v -> ((CI.mk x, CI.mk s), readDouble v)) samples xs
     (header:content) = B.lines input
     samples = tail $ B.split '\t' header
 
