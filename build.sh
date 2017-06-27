f6716b5bd4ae10f50f56b0c187b2be93dab826ba
Kai Zhang <kai@kzhang.org>
2017-06-27 15:25:21 -0700

    minor

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-cmd="cd build && stack build --allow-different-user --flag Taiji:static"
-cmd+="&& mv `stack path --allow-different-user --local-install-root`/bin/taiji taiji"
-docker run -v `pwd`:/build:rw kaizhang/haskell-stack /bin/bash -c ${cmd}
+docker run -v `pwd`:/build:rw kaizhang/haskell-stack /bin/bash -c \
+    "cd build && stack build --allow-different-user --flag Taiji:static && mv \`stack path --allow-different-user --local-install-root\`/bin/taiji taiji-Linux-x86_64-static"
